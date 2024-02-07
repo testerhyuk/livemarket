4f414b2551b74a1f60a841b76dc67c6c50509661
Hyuk <testerhyuk214@gmail.com>
2024-02-07 18:07:27 +0900

    springboot & docker 환경 설정

---
+++ src/main/java/com/market/livemarket/LivemarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.livemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivemarketApplication.class, args);
+	}
+
+}
