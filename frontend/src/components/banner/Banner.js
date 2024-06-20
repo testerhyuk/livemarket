8e2f62c609116e4fe41b14d607e0bfb8133c6cea
Hyuk <testerhyuk214@gmail.com>
2024-06-20 16:25:14 +0900

    배너 이미지 자동 슬라이드

--- frontend/src/components/banner/Banner.js
+++ frontend/src/components/banner/Banner.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import banner1 from '../../pictures/banner1.png'
+import banner2 from '../../pictures/banner2.png'
+import banner3 from '../../pictures/banner3.png'
 
 export default function Banner() {
+    const images = [banner1, banner2, banner3]
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const intervalTime = 3000;
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIndex((currentIndex + 1) % images.length);
+        }, intervalTime);
+
+        return () => clearInterval(intervalId);
+    }, [currentIndex, images.length, intervalTime]);
+
   return (
     <>
-        <img src={banner1} alt='banner1' />
+        <img src={images[currentIndex]} alt={`banner${currentIndex}`} />
     </>
   )
 }
