230f96853dc1cc028cbe7da0e9bf9815ae275e1e
Hyuk <testerhyuk214@gmail.com>
2024-08-21 16:57:15 +0900

    회원 가입 시 주소 추가, 이메일 인증 구현

--- src/main/java/com/market/livemarket/controller/MailController.java
+++ src/main/java/com/market/livemarket/controller/MailController.java
@@ -4,7 +4,6 @@ import com.market.livemarket.service.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
